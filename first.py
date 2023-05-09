fee606f524a51e7e1af8b41b3107e85cc79f4fcf
Dan <Daniel@Stanley5.com>
2023-05-08 17:49:19 -0700

    Find nearby edges

--- first.py
+++ first.py
@@ -1,6 +1,9 @@
 import numpy as np
+np.random.seed(4)
 from my_vpython import start_visualization
+from surfaces import surface_klein
 
+INF = float('inf')
 
 print('hi')
 
@@ -10,103 +13,41 @@ print('hi')
 #    [0, 1, 0]
 #])
 
-#meshgrid_inputs =
-N = 50
-us, vs = np.meshgrid(*[np.arange(0, 1, 1/N)]*2)
-uvs = np.stack((us, vs), 2).reshape((-1, 2))
+#N = 50
+#us, vs = np.meshgrid(*[np.arange(0, 1, 1/N)]*2)
+#uvs = np.stack((us, vs), 2).reshape((-1, 2))
+surface = np.vectorize(surface_klein, signature='(2)->(3)')
 
-def surface_sphere(uv):
-    u, v = uv
-    x = np.cos(2 * np.pi * u) * np.sin(np.pi * v)
-    y = np.sin(2 * np.pi * u) * np.sin(np.pi * v)
-    z = np.cos(np.pi * v)
-    return np.array([x, y, z])
+def get_dist(uv_a, uv_b, xyz_a, xyz_b):
+    # return xyz distance between things; or float(inf) if u distance is greater than 0.25
+    u_a, u_b = uv_a[0], uv_b[0]
+    if 0.25 < abs(u_b - u_a) < 0.75:
+        return INF
 
-def surface_cylinder(uv):
-    r1, r2 = 0.5, 0.2
-    u, v = uv
-    x = (r1 + r2*np.cos(2 * np.pi * v)) * np.cos(2 * np.pi * u)
-    y = (r1 + r2*np.cos(2 * np.pi * v)) * np.sin(2 * np.pi * u)
-    z = r2 * np.sin(2 * np.pi * v)
-    return np.array([x, y, z])
+    return np.sqrt(np.sum((xyz_b - xyz_a)**2))
 
-def surface_klein(uv):
-    h_top = 0.5
-    h_bottom = 1
-    w_right = 0.5
-    w_left = 0.3
-    r1 = 0.1
-    r2 = 0.4
-    r3 = 0.6
+def get_pairs(uvs):
+    xyzs = surface(uvs)
+    close = []
+    maximum = 0.15
+    for i in range(len(uvs)-1):
+        for j in range(i+1, len(uvs)):
+            uv_i, uv_j = uvs[i], uvs[j]
+            xyz_i, xyz_j = xyzs[i], xyzs[j]
+            dist = get_dist(uv_i, uv_j, xyz_i, xyz_j)
+            if dist < maximum:
+                close.append((i, j, dist))
+    print('found', len(close), 'edges')
+    return close
 
-    # difficulty with this parameterization:
-    # consider what happens as the inner tube approaches the bottom:
-    # r must be growing as u_z is at a standstill
-    # u_z must be growing as u_x is at a standstill
-    # so I don't think we can accomplish this with trig functions along
-    # solution: we will use nested trig functions for u_x to make it doubly slow
+    #for i, j, dist in close:
+    #    xyz_i, xyz_j = xyzs[i], xyzs[j]
 
-    def jug_handle(t, h_top, h_bottom, w):
-        # as t ranges from 0 to 1, this ranges from (0, h_top) to (0, -h_bottom) with a stop at (w, 0)
-        # I think h_bottom should equal 2*h_top to have continuous second derivative, and w=h_top looks good
-        if t < 0.5:
-            t0 = 2 * t
-            x = w * np.sin(1 / 2 * np.pi * t0)
-            z = h_top * np.cos(1/2 * np.pi * t0)
-        else:
-            t1 = 2 * t - 1
-            # as tu goes from 0 to 1 so does tu_stretch, but it slows at the end
-            t_stretch = np.sin(np.pi / 2 * t1)
-            x = w * (0.5 * np.cos(np.pi * t_stretch) + 0.5)
-            z = h_bottom * np.cos(1/2 * np.pi * (t1+1))
-        return x, z
+N = 500
+uvs = np.random.uniform(size=(N, 2))
 
-    def partial_f(f, t):
-        # numerical derivative of f with respect to t at t
-        eps = 1e-6
-        t_low = max(0, t-eps)
-        t_high = min(1, t+eps)
-        x_low, z_low = f(t_low)
-        x_high, z_high = f(t_high)
-        dx = (x_high - x_low) / (t_high - t_low)
-        dz = (z_high - z_low) / (t_high - t_low)
-        return dx, dz
+pairs = get_pairs(uvs)
+xyzs = surface(uvs)
+edges = [(xyzs[i], xyzs[j]) for i, j, dist in pairs]
 
-    def cylinderify(f, t, v, r):
-        x_u, z_u = f(t)
-        y_u = 0
-
-        dx, dz = partial_f(f, t)
-        ds = np.sqrt(dx**2 + dz**2)
-
-        x_v = r * np.cos(2*np.pi*v)
-        y_v = r * np.sin(2*np.pi*v)
-
-        x = x_u + x_v * (dz/ds)
-        y = y_u + y_v
-        z = z_u + x_v * (-dx/ds)
-        return x, y, z
-
-
-    u, v = uv
-    if u < 0.5:
-        # right hand jug handle
-        t = 2*u
-        f = lambda t0: jug_handle(t0, h_top, h_bottom, w_right)
-
-        r = r1 if t < 0.5 else r1 + (1-np.cos(np.pi*(t-0.5)))*(r2 - r1)
-        x, y, z = cylinderify(f, t, v, r)
-
-    else:
-        # left hand jug handle
-        t = 2*u - 1
-        f = lambda t0: jug_handle(1-t0, h_top, h_bottom, -w_left)
-
-        r = r2 + np.sin(2*np.pi*t)*(r3-r2) if t < 0.5 else r2 + np.sin(np.pi*(t-0.5))*(r1-r2)
-        x, y, z = cylinderify(f, t, v, r)
-
-
-    return np.array([x, y, z])
-
-surface = np.vectorize(surface_klein, signature='(2)->(3)')(uvs)
-start_visualization(surface)
+start_visualization(xyzs, edges)
--- my_vpython.py
+++ my_vpython.py
@@ -2,7 +2,7 @@ from vpython import *
 import time
 
 
-def start_visualization(points):
+def start_visualization(vertices, edges):
     scene.range = 1.2
     scene.caption = 'Right click + drag to rotate camera, scroll wheel to zoom'
     scene.width = 1080
@@ -10,7 +10,15 @@ def start_visualization(points):
     scene.ambient = vector(0.4, 0.4, 0.4)
 
     # by default y is locked to be straight up, but I like to use z for that
-    spheres = [sphere(pos=vector(x, z, y), radius=0.03) for x, y, z in points]
+    vs = [sphere(pos=vector(x, z, y), radius=0.03) for x, y, z in vertices]
+    es = []
+    for edge in edges:
+        ax, az, ay = edge[0]
+        bx, bz, by = edge[1]
+        dist = sum((edge[1]-edge[0])**2)**0.5
+        e = cylinder(pos=vector(ax, ay, az), axis=vector(bx-ax, by-ay, bz-az), length=dist, radius=0.01, color=vector(0,0,1))
+        es.append(e)
+
 
     #while True:
     #    #rate(60)
