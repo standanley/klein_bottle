1748cae969812b3ecab6ee4cb0ae5838b363076c
Dan <Daniel@Stanley5.com>
2023-05-08 19:31:18 -0700

    Moving vertices and animation!

--- first.py
+++ first.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 np.random.seed(4)
 from my_vpython import start_visualization
@@ -19,17 +21,18 @@ print('hi')
 surface = np.vectorize(surface_klein, signature='(2)->(3)')
 
 def get_dist(uv_a, uv_b, xyz_a, xyz_b):
-    # return xyz distance between things; or float(inf) if u distance is greater than 0.25
+    # return xyz distance between things; or float(inf) if u distance is greater than u_limit
+    # beware: if you want to add a v_limit, you need to think about the seams, and it's difficult
+    u_limit = 0.15
     u_a, u_b = uv_a[0], uv_b[0]
-    if 0.25 < abs(u_b - u_a) < 0.75:
+    if u_limit < abs(u_b - u_a) < 1-u_limit:
         return INF
 
     return np.sqrt(np.sum((xyz_b - xyz_a)**2))
 
-def get_pairs(uvs):
+def get_pairs(uvs, xyzs, maximum):
     xyzs = surface(uvs)
     close = []
-    maximum = 0.15
     for i in range(len(uvs)-1):
         for j in range(i+1, len(uvs)):
             uv_i, uv_j = uvs[i], uvs[j]
@@ -37,17 +40,109 @@ def get_pairs(uvs):
             dist = get_dist(uv_i, uv_j, xyz_i, xyz_j)
             if dist < maximum:
                 close.append((i, j, dist))
-    print('found', len(close), 'edges')
+    #print('found', len(close), 'edges')
     return close
 
     #for i, j, dist in close:
     #    xyz_i, xyz_j = xyzs[i], xyzs[j]
 
-N = 500
+def bump_uv(uv, duv):
+    # this would be simple but for wrapping behavior, particularly because of the non-orientatableness
+    u_temp = (uv[0] + duv[0]) % 2
+    u_final = u_temp % 1
+
+    v_temp = (uv[1] + duv[1]) % 1
+    v_final = 1 - v_temp if u_temp >= 1 else v_temp
+
+    return np.array([u_final, v_final])
+
+
+def jacobian(uvs):
+    # input is Nx2
+    # output is Nx3x2
+    eps = 1e-6
+
+    def _jacobian(uv):
+        ans = np.zeros((3,2))
+        for i in range(2):
+            bump_low = [0, 0]
+            bump_low[i] = -eps
+            bump_high = [0, 0]
+            bump_high[i] = eps
+            xyz_low = surface(bump_uv(uv, bump_low))
+            xyz_high = surface(bump_uv(uv, bump_high))
+            ans[:,i] = (xyz_high - xyz_low)/(2*eps)
+        return ans
+
+    ans = np.vectorize(_jacobian, signature='(2)->(3,2)')(uvs)
+    return ans
+
+
+def spring_push(uvs, xyzs, jac, edges, max_dist, k):
+    # we don't do any momentum, so there's no timestep or mass, just dx=f
+    forces_felt = np.zeros((len(uvs), 3))
+    spring_count = 0
+    total_dist = 0
+    for i, j, dist in edges:
+        if dist > max_dist:
+            continue
+        spring_count += 1
+        total_dist += dist
+        f_magnitude = k*(max_dist - dist)
+        # points from i to j
+        f_vec = (xyzs[j] - xyzs[i])/dist * f_magnitude
+        forces_felt[i] += -f_vec
+        forces_felt[j] += f_vec
+
+    print('spring count:', spring_count, '\tavg spring length', total_dist/spring_count)
+
+    uvs_new = np.zeros(uvs.shape)
+    for i in range(len(uvs)):
+        if np.sum(forces_felt**2) == 0:
+            uvs_new[i, :] = uvs[i, :]
+        else:
+            # no momentum, just move it by the force vector
+            f_xyz = forces_felt[i]
+            assert f_xyz.shape == (3,)
+            inverse_jac= np.linalg.pinv(jac[i, :, :])
+            assert inverse_jac.shape == (2, 3)
+            f_uv = inverse_jac @ f_xyz
+            assert f_uv.shape == (2,)
+            uv_new = bump_uv(uvs[i], f_uv)
+            uvs_new[i, :] = uv_new
+
+    return uvs_new
+
+
+N = 200
 uvs = np.random.uniform(size=(N, 2))
 
-pairs = get_pairs(uvs)
-xyzs = surface(uvs)
-edges = [(xyzs[i], xyzs[j]) for i, j, dist in pairs]
+spring_max_length = 0.3
+spring_k = 0.05
 
-start_visualization(xyzs, edges, uvs[:,0])
+print('starting callback')
+callback = start_visualization()
+print('finished callback')
+
+for i in range(200):
+    jac = jacobian(uvs)
+    xyzs = surface(uvs)
+    print('starting pairs')
+    pairs = get_pairs(uvs, xyzs, spring_max_length)
+    print('finished pairs')
+    edges = [(xyzs[i], xyzs[j]) for i, j, dist in pairs]
+    callback(xyzs, edges, uvs[:, 0])
+    if i == 0:
+        print('sleeping 10 seconds to let vpython load better')
+        time.sleep(10)
+
+    uvs = spring_push(uvs, xyzs, jac, pairs, spring_max_length, spring_k)
+
+xyzs = surface(uvs)
+pairs = get_pairs(uvs, xyzs, spring_max_length)
+edges = [(xyzs[i], xyzs[j]) for i, j, dist in pairs]
+callback(xyzs, edges, uvs[:,0])
+
+np.savetxt('uvs_moved.csv', uvs, delimiter=',')
+uvs = np.loadtxt('uvs_moved.csv', delimiter=',')
+
--- my_vpython.py
+++ my_vpython.py
@@ -2,36 +2,57 @@ from vpython import *
 import time
 
 
-def start_visualization(vertices, edges, vertex_coloring=None):
+def start_visualization():
     scene.range = 1.2
     scene.caption = 'Right click + drag to rotate camera, scroll wheel to zoom'
     scene.width = 1080
     scene.height = 1080
     scene.ambient = vector(0.4, 0.4, 0.4)
 
-    # by default y is locked to be straight up, but I like to use z for that
     vs = []
-    for i, vertex in enumerate(vertices):
-        r = 0.03
-        x, z, y = vertex
-        pos = vector(x, y, z)
-        if vertex_coloring is None:
-            v = sphere(pos=pos, radius=r)
-        else:
-            c = vertex_coloring[i]
-            v = sphere(pos=pos, radius=r, color=vector(c, 0, 1-c))
-        vs.append(v)
-
     es = []
-    for edge in edges:
-        ax, az, ay = edge[0]
-        bx, bz, by = edge[1]
-        dist = sum((edge[1]-edge[0])**2)**0.5
-        e = cylinder(pos=vector(ax, ay, az), axis=vector(bx-ax, by-ay, bz-az), length=dist, radius=0.01, color=vector(0,1,0))
-        es.append(e)
 
+    def callback(vertices, edges, vertex_coloring=None):
+        if len(vertices) > len(vs):
+            for i in range(len(vertices) - len(vs)):
+                vs.append(sphere())
+        if len(edges) > len(es):
+            for i in range(len(edges) - len(es)):
+                es.append(cylinder())
 
-    #while True:
-    #    #rate(60)
-    #    time.sleep(1)
+        # by default y is locked to be straight up, but I like to use z for that
+        for i, v in enumerate(vs):
+            if i >= len(vertices):
+                v.visible = False
+                continue
+            vertex = vertices[i]
 
+            r = 0.03
+            x, z, y = vertex
+            pos = vector(x, y, z)
+
+            v.pos = pos
+            v.radius = r
+            if vertex_coloring is None:
+                v.color = vector(1,1,1)
+            else:
+                c = vertex_coloring[i]
+                v.color = vector(c, 0, 1-c)
+
+        for i, e in enumerate(es):
+            if i >= len(edges):
+                e.visible = False
+                continue
+            edge = edges[i]
+
+            ax, az, ay = edge[0]
+            bx, bz, by = edge[1]
+            dist = sum((edge[1]-edge[0])**2)**0.5
+
+            e.pos = vector(ax, ay, az)
+            e.axis = vector(bx-ax, by-ay, bz-az)
+            e.length = dist
+            e.radius = 0.01
+            e.color = vector(0,1,0)
+
+    return callback
